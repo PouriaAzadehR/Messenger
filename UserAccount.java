39bc6a3003d1aaef2c0951e437a0bd2f2f8703ed
PouriaAzadehR <pouriazadeh81@aut.ac.ir>
2021-11-28 18:39:15 +0330

    first commit

---
+++ UserAccount.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * this is a class which is used for each user in order to have access to tweeter app
+ * it contains username biography and password
+ * @author pouria
+ * @version 1
+ * @since 11/28/2021
+ */
+public class UserAccount {
+}
