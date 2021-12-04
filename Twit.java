c82689b4fa849621e846bcbf75ae9e43cc2a0d49
PouriaAzadehR <pouriazadeh81@aut.ac.ir>
2021-12-04 19:04:25 +0330

    twit class created

---
+++ Twit.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * this is a class for twits which are posted by users
+ *
+ * @author pouri
+ * @version 1
+ * @since today
+ */
+public class Twit {
+
+    private UserAccount sender;
+
+
+}
