811c50de88385b38508a051836a0de6ed750b661
PouriaAzadehR <pouriazadeh81@aut.ac.ir>
2021-12-24 18:39:13 +0330

    out and in fields are reponsible field for cominucating with server

--- CommandParser.java
+++ CommandParser.java
@@ -1,6 +1,17 @@
 package com.company;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * this class is like the manger of client and is responsible for communicating with server
+ * @author pouri
+ * @version 1
+ * @since today
+ */
 public class CommandParser {
 
-    String req;
+    private String req;
+    private OutputStream out;
+    private InputStream in;
 }
