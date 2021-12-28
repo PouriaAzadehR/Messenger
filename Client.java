00348c502f3898aeff57eb15eeffc6b41718cf36
PouriaAzadehR <pouriazadeh81@aut.ac.ir>
2021-12-28 15:02:43 +0330

    refactored

--- Client.java
+++ Client.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * this is a class for client which want to connect to server
@@ -14,10 +16,18 @@ import java.net.Socket;
 public class Client {
 
     public static void main(String[] args) throws IOException {
+        Scanner scanner=new Scanner(System.in);
+        ConsoleviewService consoleviewService=new ConsoleviewService();
         try (Socket client = new Socket("127.0.0.1", 7660)) {
-            System.out.println("Connected to server.");
             CommandParser commandParser =new CommandParser(client.getOutputStream(),client.getInputStream());
-            System.out.println("done.");
+            //signIn sign up
+           commandParser.signInSignUp();
+           commandParser.stop();
+            while (true){
+               commandParser.choose();
+               commandParser.stop();
+            }
+            //consoleviewService.done();
         }
     }
 }
